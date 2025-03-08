5c971960bb66feffb5e628ef2adc23449663de94
Nayereh Afiuny <88449572+nafiuny@users.noreply.github.com>
2025-03-08 23:57:19 +0330

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,22 +1,15 @@
+import zipfile
 import os
-import gdown
 
 
-file_urls = [
-    "test_data.pt":   "https://drive.google.com/file/d/1_1bBU9HdHkXWVlFf3_6XKM-b91soBkzY/view?usp=sharing", 
-    "train_data.pt":  "hhttps://drive.google.com/file/d/1FX6hqOOOT9ecEf2bZtISZmEz9Xg096EG/view?usp=sharing", 
-    "val_data.pt":    "https://drive.google.com/file/d/17vkZUrEnC17XBJRtuMPTGM32jUZlyggv/view?usp=sharing"    
-]
-
-output_dir = 'outputs/data'
-
+dataset_file_path = 'brain-tumor-mri-dataset.zip' 
+output_dir = 'Brain_Tumor_MRI_Dataset'  
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# دانلود هر فایل
-for file_name, url in file_urls.items():
-    output_path = os.path.join(output_dir, file_name)
-    print(f"Downloading {file_name}...")
-    gdown.download(url, output_path, quiet=False)
+# extract
+with zipfile.ZipFile(dataset_file_path, 'r') as zip_ref:
+    zip_ref.extractall(output_dir)
 
-print("Dataset download completed!")
+print(f"Dataset has been extracted to: {output_dir}")
+
